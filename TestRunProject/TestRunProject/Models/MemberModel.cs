cbb2ea4e66b670dd01e5d57d716fb8ab222f1d79
Harry Xiao <shawhu@gmail.com>
2015-08-03 14:46:26 +0800

    nickname

--- TestRunProject/TestRunProject/Models/MemberModel.cs
+++ TestRunProject/TestRunProject/Models/MemberModel.cs
@@ -9,6 +9,7 @@ namespace TestRunProject
     {
         public int member_id { get; set; }
         public string email { get; set; }
+        public string nick_name { get; set; }
         public MemberModel()
         {
 
